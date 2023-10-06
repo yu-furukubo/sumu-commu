774dc2c2792939f2df496e1686aa136eaaebbc62
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-10-06 04:14:43 +0000

    サインアップ時のカラム追加、seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  name: 'テスト太郎',
+  email: 'admin@test',
+  password: '111111'
+  )
+
+Residence.create!(
+  admin_id: '1',
+  housing_type: '0',
+  name: 'テストマンション',
+  address: '東京都架空市適当ヶ丘1-2-3',
+  )
+
+Member.create!(
+  residence_id: '1',
+  name: 'テストメンバー',
+  house_address: '101',
+  email: 'member@test',
+  password: '111111'
+  )

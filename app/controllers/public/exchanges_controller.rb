4e6d720a5d270216e2868bfc1e00bfd2aad5b631
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-10-14 05:21:13 +0000

    会員落とし物ゆずりあい機能

--- app/controllers/public/exchanges_controller.rb
+++ app/controllers/public/exchanges_controller.rb
@@ -2,14 +2,63 @@ class Public::ExchangesController < ApplicationController
   before_action :authenticate_member!
 
   def index
+    @exchanges = Exchange.where(residence_id: current_member.residence.id)
+    @exchanges_mine = @exchanges.where(member_id: current_member.id)
+    @exchanges_others = @exchanges.where.not(member_id: current_member.id)
   end
 
   def show
+    @exchange = Exchange.find(params[:id])
+    @exchange_comments = ExchangeComment.where(exchange_id: @exchange.id).order(created_at: "DESC")
+    @exchange_comment = ExchangeComment.new
   end
 
   def new
+    @exchange = Exchange.new
+  end
+
+  def create
+    @exchange = Exchange.new(exchange_params)
+    if @exchange.save
+      redirect_to public_exchange_path(@exchange)
+    else
+      render "new"
+    end
   end
 
   def edit
+    @exchange = Exchange.find(params[:id])
   end
+
+  def update
+    @exchange = Exchange.find(params[:id])
+    if params[:exchange][:image_ids]
+      params[:exchange][:image_ids].each do |image_id|
+        image = @exchange.exchange_item_images.find(image_id)
+        image.purge
+      end
+    end
+    if @exchange.update(exchange_params)
+      redirect_to public_exchange_path(@exchange)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    exchange = Exchange.find(params[:id])
+    if exchange.destroy
+      redirect_to public_exchanges_path
+    else
+      flash.now[:notice] = "削除に失敗しました"
+      render "show"
+    end
+  end
+
+  private
+
+  def exchange_params
+    params.require(:exchange).permit(:name, :description, :price, :deadline, :is_finished, :member_id, :residence_id, exchange_item_images: [])
+  end
+
 end
--- app/controllers/public/lost_item_comments_controller.rb
+++ app/controllers/public/lost_item_comments_controller.rb
@@ -1,4 +1,43 @@
 class Public::LostItemCommentsController < ApplicationController
   before_action :authenticate_member!
 
+  def create
+    lost_item = LostItem.find(params[:lost_item_id])
+    lost_item_comment = LostItemComment.new(lost_item_comment_params)
+    if lost_item_comment.save
+      redirect_to public_lost_item_path(lost_item)
+    else
+      flash.now[:notice] = "コメントの投稿に失敗しました"
+      render template: "public/lost_items/show"
+    end
+  end
+
+  def update
+    lost_item = LostItem.find(params[:lost_item_id])
+    lost_item_comment = LostItemComment.find(params[:id])
+    if lost_item_comment.update(lost_item_comment_params)
+      redirect_to public_lost_item_path(lost_item)
+    else
+      flash.now[:notice] = "コメントの更新に失敗しました"
+      render template: "public/lost_items/show"
+    end
+  end
+
+  # def destroy
+  #   lost_item = LostItem.find(params[:lost_item_id])
+  #   lost_item_comment = LostItemComment.find(params[:id])
+  #   if lost_item_comment.destroy
+  #     redirect_to public_lost_item_path(lost_item)
+  #   else
+  #     flash.now[:notice] = "更新に失敗しました"
+  #     render template: "public/lost_items/show"
+  #   end
+  # end
+
+  private
+
+  def lost_item_comment_params
+    params.require(:lost_item_comment).permit(:lost_item_id, :member_id, :comment, :is_deleted)
+  end
+
 end

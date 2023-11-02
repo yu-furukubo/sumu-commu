460d8345feb341b8099b4f2f39db1fcf84fadc3d
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-11-02 07:46:07 +0000

    管理側非同期化

--- app/controllers/admin/event_members_controller.rb
+++ app/controllers/admin/event_members_controller.rb
@@ -12,11 +12,11 @@ class Admin::EventMembersController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
     event_member = EventMember.new(event_member_params)
-    if event_member.save
-      redirect_to admin_event_event_members_path(@event)
-    else
+    @residence_members = @event.residence.members
+    @event_members = @event.event_members.where(is_approved: true)
+    @event_invited_members = @event.event_members.where(is_approved: false)
+    unless event_member.save
       flash.now[:alert] = "イベントへの招待に失敗しました。"
-      @members = @event.residence.members
       render "index"
     end
   end
@@ -24,28 +24,27 @@ class Admin::EventMembersController < ApplicationController
   def invite_all
     @event = Event.find(params[:event_id])
     @residence_members = @event.residence.members
+    @event_members = @event.event_members.where(is_approved: true)
+    @event_invited_members = @event.event_members.where(is_approved: false)
     @residence_members.each do |member|
       if not EventMember.find_by(event_id: @event.id, member_id: member.id, is_approved: true).present?
         unless EventMember.find_or_create_by(event_id: @event.id, member_id: member.id, is_approved: false)
           flash.now[:alert] =　"#{member}の招待に失敗しました。"
-          @event_members = @event.event_members.where(is_approved: true)
-          @event_invited_members = @event.event_members.where(is_approved: false)
           render "index"
           return
         end
       end
     end
-    redirect_to admin_event_event_members_path(@event)
   end
 
   def quit_invite
     @event = Event.find(params[:event_id])
     event_member = EventMember.find(params[:id])
-    if event_member.destroy
-      redirect_to admin_event_event_members_path(@event)
-    else
+    @residence_members = @event.residence.members
+    @event_members = @event.event_members.where(is_approved: true)
+    @event_invited_members = @event.event_members.where(is_approved: false)
+    unless event_member.destroy
       flash.now[:alert] = "イベント招待の削除に失敗しました"
-      @members = @event.residence.members
       render "index"
     end
   end

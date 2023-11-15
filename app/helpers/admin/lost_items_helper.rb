abb44e76a4768f40f1a73a91dbf3793810b9e1ca
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-11-15 14:43:00 +0000

    ヘルパーメソッド削除

--- app/helpers/admin/lost_items_helper.rb
+++ app/helpers/admin/lost_items_helper.rb
@@ -1,5 +1,5 @@
 module Admin::LostItemsHelper
-  def my_link_to(id)
-    residence_search_admin_lost_items_path"#{id}"
-  end
+  # def my_link_to(id)
+  #   residence_search_admin_lost_items_path"#{id}"
+  # end
 end
--- app/helpers/admin/members_helper.rb
+++ app/helpers/admin/members_helper.rb
@@ -1,5 +1,5 @@
 module Admin::MembersHelper
-  def my_link_to(id)
-    residence_search_admin_members_path"#{id}"
-  end
+  # def my_link_to(id)
+  #   residence_search_admin_members_path"#{id}"
+  # end
 end
